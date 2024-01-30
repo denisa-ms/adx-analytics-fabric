886c4df341699d93d69756a6a28f1caef42b8c8c
denisa-ms <dschlesinger@microsoft.com>
2024-01-30 17:26:52 +0200

    save

---
+++ infrastructure scripts/createAll.ps1
@@ -0,0 +1,14 @@
+# Connect-AzAccount
+# Subscription id of the current subscription
+$subscriptionId=$(az account show --query id --output tsv)
+$resourceGroupName = "adx-fabric-rg"
+$location = "westeurope"
+
+
+# Set subscription 
+Set-AzContext -SubscriptionId $subscriptionId 
+# Create a resource group
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile deployAll.bicep -WarningAction:SilentlyContinue
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile .\ADF_pipeline2.json -WarningAction:SilentlyContinue
